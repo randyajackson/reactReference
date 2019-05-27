31c8122a508b2ba4b5228c2050f04657c69332a6
randyajackson <rajackson.dev@gmail.com>
2019-05-26 21:05:37 -0600

    5/26/19 9p

---
+++ Section 3 Understanding Basics/section3/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() { // this is the same as class App extends Componenet { render(){ return(...) } }
+  return ( // this is not html, it is jsx
+    <div className="App"> 
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
--- Section 3 Understanding Basics/section3Notes.js
+++ Section 3 Understanding Basics/section3Notes.js
@@ -1,4 +1,29 @@
 /**
+ * npm install -g create-react-app
+ * navigate into dir created
+ * npm start
+ * 
+ * dependencies are in package.json file
+ * 
+ * npm start
+ * npm build
+ * npm test
+ * npm eject
+ * 
+ * public folder contains files that are seen publicly
+ * 
+ * <div id="root"></div> is where react application is mounted in index.html
+ * 
+ * src folder contains react application
+ * 
+ * app is rendered in the index.js file
+ * 
+ * mostly working in app.js
+ * 
+ * only one root rendered
+ * 
+ * ----------------------------notes in app.js
+ * 
  * 
  * 
  */

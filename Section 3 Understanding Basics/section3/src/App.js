9b5dfe5372588d781094f875d82cb3877244e712
randyajackson <rajackson.dev@gmail.com>
2019-06-02 14:23:06 -0600

    6/2/19 2:30p

--- Section 3 Understanding Basics/section3/src/App.js
+++ Section 3 Understanding Basics/section3/src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-function App() { // this is the same as class App extends Componenet { render(){ return(...) } }
+class App extends Component { // this is the same as class App extends Componenet { render(){ return(...) } }
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 26}
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was clicked');
+    //this.state.persons[0].name = 'Maximilian'; don't change the state this way
+    this.setState({persons: [
+        { name: 'Maximilian', age: 28},
+        { name: 'Manu', age: 29},
+        { name: 'Stephanie', age: 31}
+    ] })
+  }
+
+  render() {
   return ( // this is not html, it is jsx
     //class is html, className is jsx, className renders to class through react
     <div className="App"> 
       <h1>Test</h1>
-      <Person  name="Max" age="28"/> 
-      <Person  name="Manu" age="29">My Hobbies: Racing</Person>
-      <Person  name="Stephanie" age="26"/>
+      <button onClick={this.switchNameHandler}>Switch</button>
+      <Person  name={this.state.persons[0].name} age={this.state.persons[0].age}/> 
+      <Person  name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+      <Person  name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
     //name and age are properties or "props"
+     //in JSX it is onClick
+
+    //changing the state causes the data in the persons array to be re-rendered
 
     //good practice to wrap everything in one root component
 
@@ -21,6 +44,7 @@ function App() { // this is the same as class App extends Componenet { render(){
     //<Person /> makes it easy to reuse code
     
   );
+  }
 }
 
 export default App;

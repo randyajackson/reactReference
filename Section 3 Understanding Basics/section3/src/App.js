abb0e6d9262f0f63a68261a8dcfd64b1b4944731
randyajackson <rajackson.dev@gmail.com>
2019-06-02 15:29:52 -0600

    6/2/19 3:30p

--- Section 3 Understanding Basics/section3/src/App.js
+++ Section 3 Understanding Basics/section3/src/App.js
@@ -1,40 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-class App extends Component { // this is the same as class App extends Componenet { render(){ return(...) } }
-  state = {
-    persons: [
-      { name: 'Max', age: 28},
+const App = props => { // this is the same as class App extends Componenet { render(){ return(...) } }
+//using react hooks, useState allows us to manage states
+//if you use hooks you do not need class based components, but using class is the established way of react development.
+
+//stateful component, a component that manages state
+//Person.js is a stateless component because they do not update the state
+//stateful is "smart" component
+//if there are a lot of stateful components, managing code becomes difficult
+
+const [personsState, setPersonsState] = useState({
+  persons: [
+    { name: 'Max', age: 28},
+    { name: 'Manu', age: 29},
+    { name: 'Stephanie', age: 26}
+  ],
+  otherState: 'some other value'
+});
+
+const switchNameHandler = (newName) => {
+  //console.log('Was clicked');
+  //this.state.persons[0].name = 'Maximilian'; don't change the state this way
+  setPersonsState({persons: [
+      { name: newName, age: 28},
       { name: 'Manu', age: 29},
-      { name: 'Stephanie', age: 26}
-    ],
-    otherState: 'some other value'
-  }
+      { name: 'Stephanie', age: 31}
+  ] });
+};
 
-  switchNameHandler = () => {
-    //console.log('Was clicked');
-    //this.state.persons[0].name = 'Maximilian'; don't change the state this way
-    this.setState({persons: [
-        { name: 'Maximilian', age: 28},
-        { name: 'Manu', age: 29},
-        { name: 'Stephanie', age: 31}
-    ] })
-  }
+const nameChangedHandler = (event) => {
+  setPersonsState({persons: [
+    { name: 'Max', age: 28},
+    { name: event.target.value, age: 29},
+    { name: 'Stephanie', age: 26}
+] });
+};
 
-  render() {
+const style = {
+  backgroundColor: 'white',
+  font: 'inherit',
+  border: '1x solid blue',
+  padding: '8px',
+  cursor: 'pointer'
+}
   return ( // this is not html, it is jsx
     //class is html, className is jsx, className renders to class through react
     <div className="App"> 
       <h1>Test</h1>
-      <button onClick={this.switchNameHandler}>Switch</button>
-      <Person  name={this.state.persons[0].name} age={this.state.persons[0].age}/> 
-      <Person  name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-      <Person  name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <button style={style}
+      onClick={ () => switchNameHandler('Maximillian!!')}>Switch</button>
+      <Person  name={personsState.persons[0].name} age={personsState.persons[0].age}/> 
+      <Person  name={personsState.persons[1].name} age={personsState.persons[1].age} click= {switchNameHandler.bind(this, 'Max!')} changed= {nameChangedHandler}>My Hobbies: Racing</Person>
+      <Person  name={personsState.persons[2].name} age={personsState.persons[2].age}/>
     </div>
     //name and age are properties or "props"
      //in JSX it is onClick
+
+     //you can pass methods as props
+
 
     //changing the state causes the data in the persons array to be re-rendered
 
@@ -44,7 +70,8 @@ class App extends Component { // this is the same as class App extends Componene
     //<Person /> makes it easy to reuse code
     
   );
-  }
 }
 
 export default App;
+
+
--- Section 3 Understanding Basics/section3/src/Person/Person.js
+++ Section 3 Understanding Basics/section3/src/Person/Person.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import './Person.css'; // you have to import the css file in order to use it.
+
 const person = (props) => { // props are properties from app.js
     //ES6 way to create a function
     return(
-    <div>
-        <p>I'm {props.name} person and I am {props.age} years old!!</p>
+    <div className="Person">
+        <p onClick = {props.click}>I'm {props.name} person and I am {props.age} years old!!</p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}/>
     </div>
     );
     //need to import react as return is a react function
